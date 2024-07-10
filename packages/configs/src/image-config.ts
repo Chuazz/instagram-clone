8679821b8080b6a603e25a62db0485019d4cdb8e
sonnv <sonnv1912@gmail.com>
2024-07-10 09:02:53 +0700

    chore: remove not use

--- packages/configs/src/image-config.ts
+++ packages/configs/src/image-config.ts
@@ -1,15 +1,19 @@
 import SocialTextLogoImage from '@super-app/assets/images/img_social_text_logo.png';
-import Test1Image from '@super-app/assets/images/img_test_1.jpg';
-import Test2Image from '@super-app/assets/images/img_test_2.jpg';
 import PictureIcon from '@super-app/assets/icons/ic_picture.png';
 import StoryBorderIcon from '@super-app/assets/icons/ic_story_border.png';
+import AppLogoIcon from '@super-app/assets/icons/ic_app_logo.png';
+import AppIcon from '@super-app/assets/icons/ic_app.png';
+import LoginBackgroundImage from '@super-app/assets/images/img_login_background.png';
+import ContributorImage from '@super-app/assets/images/img_contributor.png';
 
 const image = {
 	SocialTextLogoImage,
-	Test1Image,
-	Test2Image,
 	PictureIcon,
 	StoryBorderIcon,
+	AppLogoIcon,
+	AppIcon,
+	LoginBackgroundImage,
+	ContributorImage,
 };
 
 export { image };
