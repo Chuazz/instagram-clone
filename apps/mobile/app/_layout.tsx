5df30160e52902125b244a3dfb5811bd7567253a
sonnv <sonnv1912@gmail.com>
2024-07-10 09:09:08 +0700

    feat: config reactogon

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,30 +1,42 @@
 import { SplashScreen } from '@/components/layout';
 import { AppNavigation } from '@/components/navigation';
 import { TamaguiProvider } from '@/providers/tamagui-provider';
+import { LegendState } from '@super-app/components/src';
 import { ReactQueryProvider } from '@super-app/providers/src/react-query-provider';
 import { StatusBar } from 'react-native';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { enableReactComponents } from '@legendapp/state/config/enableReactComponents';
+import Reactotron, { asyncStorage } from 'reactotron-react-native';
 
-enableReactComponents();
+if (__DEV__) {
+	Reactotron.configure({
+		name: 'instagram-clone',
+	})
+		.useReactNative()
+		.use(asyncStorage())
+		.connect();
+}
 
 const RootLayout = () => {
 	return (
-		<ReactQueryProvider>
-			<SafeAreaProvider>
-				<SplashScreen>
-					<StatusBar
-						backgroundColor='black'
-						barStyle='light-content'
-					/>
+		<>
+			<LegendState />
 
-					<TamaguiProvider>
-						<AppNavigation />
-					</TamaguiProvider>
-				</SplashScreen>
-			</SafeAreaProvider>
-		</ReactQueryProvider>
+			<ReactQueryProvider>
+				<SafeAreaProvider>
+					<SplashScreen>
+						<StatusBar
+							backgroundColor='black'
+							barStyle='light-content'
+						/>
+
+						<TamaguiProvider>
+							<AppNavigation />
+						</TamaguiProvider>
+					</SplashScreen>
+				</SafeAreaProvider>
+			</ReactQueryProvider>
+		</>
 	);
 };
 
