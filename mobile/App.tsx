e7adb13decf3209b471117f4ffeadd173e405f66
sonnv <sonnv1912@gmail.com>
2024-08-10 17:36:30 +0700

    fix: remove reload button

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,12 +5,10 @@ import { trans } from '@/utils';
 import { observer } from '@legendapp/state/react';
 import { DripsyProvider, Text } from 'dripsy';
 import { StatusBar } from 'expo-status-bar';
-import { reloadAsync } from 'expo-updates';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AppBottomSheet } from './components/bottom-sheet/app-bottom-sheet';
-import { Button } from './components/form/button';
+import { AppBottomSheet } from './components/bottom-sheet/app-bottom-sheet/app-bottom-sheet';
 import { Navigation } from './components/layout/navigation';
 import { SplashScreen } from './components/layout/splash-screen';
 import { ReactQueryProvider } from './providers/react-query-provider';
@@ -24,24 +22,6 @@ const App = observer(() => {
                         <StatusBar style='dark' />
 
                         <SplashScreen>
-                            {__DEV__ && (
-                                <Button
-                                    content='reload'
-                                    size='sm'
-                                    rounded={false}
-                                    sx={{
-                                        position: 'absolute',
-                                        bottom: 0,
-                                        right: 0,
-                                        zIndex: 9999,
-                                    }}
-                                    onPress={async () => {
-                                        await reloadAsync();
-                                    }}
-                                />
-                            )}
-                            <AppBottomSheet />
-
                             <Text
                                 sx={{
                                     display: 'none',
@@ -51,7 +31,9 @@ const App = observer(() => {
                             </Text>
 
                             <ModalProvider>
-                                <Navigation />
+                                <Navigation>
+                                    <AppBottomSheet />
+                                </Navigation>
                             </ModalProvider>
                         </SplashScreen>
                     </SafeAreaProvider>
